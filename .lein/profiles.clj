de08add5f097cc43aaa82140bfb22bd1d7669a57
Martin Barksten <martin.barksten@gmail.com>
2016-03-02 13:27:27 +0100

    Initial dotfiles setup

---
+++ .lein/profiles.clj
@@ -0,0 +1,7 @@
+{:user {:plugins [[lein-ancient "0.6.5"]
+                  [lein-plz "0.4.0-SNAPSHOT" :exclusions [[rewrite-clj] [ancient-clj]]]
+                  [lein-exec "0.3.6"]
+                  [lein-search "0.3.4"]
+                  [lein-kibit "0.1.2"]
+                  [lein-try "0.4.3"]
+                  [cider/cider-nrepl "0.10.2"]]}}
